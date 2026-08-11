e75574e01b4641be120d580b0b9dab797a7a0ecb
MattCumbo99 <mattcumbo99@gmail.com>
2026-08-11 15:21:33 -0500

    Chess notation

---
+++ src/game/ChessGame.java
@@ -0,0 +1,16 @@
+package game;
+
+import board.Board;
+
+public class ChessGame {
+    private final Board board;
+
+    public ChessGame() {
+        board = new Board();
+        board.initialize();
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+}
---
+++ src/game/InvalidNotationException.java
@@ -0,0 +1,7 @@
+package game;
+
+public class InvalidNotationException extends RuntimeException {
+    public InvalidNotationException(String message) {
+        super(message);
+    }
+}
